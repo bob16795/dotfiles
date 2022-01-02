66c28ba8d3340c8cbb96166114c085ea139ab6ad
bob16795 <prestosilver.ptp@gmail.com>
2022-01-02 10:57:42 +0000

    updates

--- dotfiles/scr/reload.sh
+++ dotfiles/scr/reload.sh
@@ -5,24 +5,14 @@ dunstmerge
 
 xrdb -merge ~/.Xresources
 
-polybar-msg cmd restart
-conmanmsg reload
-
-#kill -HUP $(pidof dwm)
-
 xsetroot -name "fsignal:1"
 
 killall -v -USR1 st cava
 killall -v dunst
 killall xeyes && xeyes +shape -outline "$(mondo -c bg)" &
 
-#i3listen.py > /dev/null&
-
 dunst -q &
 
-#codereload
-
-#i3fyra -l 'AB=-450 BD=300 AC=171'
 xsetroot -name "fsignal:1"
 
 filesort &
@@ -31,4 +21,3 @@ tail -f /tmp/xobb | xob -m 3000 -s backlight &
 
 volume-watcher | xob &
 mic-watcher | xob -s mic &
-picom --experimental-backend
